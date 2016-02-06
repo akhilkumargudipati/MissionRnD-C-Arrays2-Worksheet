bfc514c2675f4a2de1b5b0c5e357516a47b35ff8
akhilkumargudipati <akhilkumargudipati@missionrnd.com>
2016-02-06 10:06:52 +0530

    Submit Commit

--- src/countGreaterNumbers.cpp
+++ src/countGreaterNumbers.cpp
@@ -13,13 +13,57 @@ ERROR CASES: Return NULL for invalid inputs.
 
 NOTES:
 */
-
+#include<stdio.h>
+int compare_strings(char a[], char b[]);
 struct transaction {
 	int amount;
 	char date[11];
 	char description[20];
 };
 
-int countGreaterNumbers(struct transaction *Arr, int len, char *date) {
-	return -1;
+int countGreaterNumbers(struct transaction *Arr, int len, char *date) 
+{
+	int temp1, count = 0, flag, temp2,flag1;
+	for (temp2 = 0; temp2 < len; temp2++)
+	{
+		flag1=compare_strings(Arr[temp2].date, Arr[temp2 + 1].date);
+		if (flag1 == 0)
+			return 0;
+	}
+
+	
+	for (temp1 = 0; temp1 < len; temp1++)
+	{
+		flag = compare_strings(Arr[temp1].date, date);
+		count++;
+		if (flag == 0)
+			break;
+            
+	}
+	if (flag != 0)
+		return NULL;
+	if (count == len&&flag == 0)
+		return 0;
+	else if (flag==0)
+	{
+		return len - count;
+	}
+	
+	
+	
+}
+int compare_strings(char a[], char b[])
+{
+	int c = 0;
+
+	while (a[c] == b[c]) {
+		if (a[c] == '\0' || b[c] == '\0')
+			break;
+		c++;
+	}
+
+	if (a[c] == '\0' && b[c] == '\0')
+		return 0;
+	else
+		return -1;
 }
